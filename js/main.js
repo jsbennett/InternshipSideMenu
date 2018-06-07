a267395317ba1a5ea185f3607ede50e714b29218
Ewan Corbett <Ewan Corbett@cw-data.local>
2018-06-07 12:23:01 +0100

    Added menu and user object

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 const childItems = {
     name: null
-}
+};
 
 const parentItems = {
     name: null,
     icon: null,
     childItems: [childItems]
-}
+};
+
+const user = {
+	admin: null,
+	chargemaster: null,
+	issues: null,
+	supply: null
+};
+
+const menu = {
+	expanded: true,
+	overlay: false,
+	tabsList: null,
+	currentTab: null
+};
