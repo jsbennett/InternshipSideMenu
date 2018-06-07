c57cc4b56f88bb27dc6d096c79106abcb29a5072
Conor Haining <Conor Haining@cw-data.local>
2018-06-07 13:20:08 +0100

    Item Creating

--- js/main.js
+++ js/main.js
@@ -61,6 +61,69 @@ function createData()
         
         menu.tabsList.push(tab);
     }
+
+    var itemNames = [
+        [
+            'Global Dashboard',
+            'Online Reference Toolkit',
+            'Chargemaster',
+            'Issues',
+            'Supply',
+        ],
+        [
+            'User Manager',
+            'User Groups',
+            'Payor Mapping',
+            'Field Mapping',
+            'Reports',
+            'Import Logs',
+        ],
+        [
+            'User Guides',
+            'Company',
+            'Legal',
+            'Privacy Policy',
+        ],
+    ];
+
+    var itemIcons = [
+        [
+            '',
+            '',
+            '',
+            '',
+            '',
+        ],
+        [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ],
+        [
+            '',
+            '',
+            '',
+            '',
+        ],
+    ];
+
+    for (var i = 0; i < 3; i++)
+    {
+        var parentItems = [];
+        for (var j = 0; j < itemNames[i].length; j++)
+        {
+            var parentItem = Object.create(parentItemsTemplate);
+            parentItem.name = itemNames[i][j];
+            parentItem.icon = itemIcons[i][j];
+
+            parentItems.push(parentItem);
+        }
+
+        menu.tabsList[i].listOfItems = parentItems;
+    }
 }
 
 function populateTabs(menu)
@@ -91,5 +154,7 @@ function showParent(tab)
 $(document).ready(function(){
 
     createData();
+
+    console.log(menu);
 
 });
